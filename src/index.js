abb297087e3f865800e78ecfc4f9021c4401ef91
ZhukMax <zhukmax@ya.ru>
2017-01-18 12:54:48 +0100

    Add Redux, init store.

--- src/index.js
+++ src/index.js
@@ -1,12 +1,18 @@
 import React      from 'react';
 import ReactDOM   from 'react-dom';
 import { browserHistory, Router } from 'react-router';
+import { Provider } from 'react-redux';
 import routes from './routes';
+import configureStore from './redux/configureStore';
+
+const store = configureStore();
 
 const feapApp = (
-    <Router history={browserHistory}>
-        {routes}
-    </Router>
+    <Provider store={store}>
+        <Router history={browserHistory}>
+            {routes}
+        </Router>
+    </Provider>
 );
 
 ReactDOM.render(feapApp, document.getElementById('feap-root'));
