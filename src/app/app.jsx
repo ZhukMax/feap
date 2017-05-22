d39092f9b471a04f416265899abbe646fe7d2a97
Max Zhuk <zhukmax@ya.ru>
2017-05-23 00:55:03 +0300

    Create app.jsx

---
+++ src/app/app.jsx
@@ -0,0 +1,50 @@
+import React from 'react';
+import Login from '../components/login';
+import Header from '../components/header';
+import Sidebar from '../components/sidebar';
+import Footer from '../components/footer';
+import { connect } from 'react-redux';
+
+import 'bootstrap/dist/css/bootstrap.css';
+import './app.css';
+
+const mapStateToProps = (state) => {
+    return {
+        token: state.auth.token
+    };
+};
+
+class App extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+
+    componentWillMount() {
+        document.body.setAttribute("class", "feap");
+    }
+
+    render() {
+        let settings = this.props.route.settings;
+        const { token } = this.props;
+        if (token) {
+            return(
+                <div>
+                    <Login />
+                </div>
+            );
+        } else {
+            return(
+                <div>
+                    <Sidebar/>
+                    <Header navigationItems={ settings.navigationItems }/>
+                    {this.props.children}
+                    <Footer/>
+                </div>
+            );
+        }
+    }
+}
+
+App = connect(mapStateToProps)(App);
+
+export default App;
