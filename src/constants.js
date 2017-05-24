3076b5121e7ad66f871d21804ea3f38765738b3a
Max Zhuk <zhukmax@ya.ru>
2017-05-25 01:37:50 +0300

    Update constants.js

--- src/constants.js
+++ src/constants.js
@@ -1,10 +1,32 @@
-export const USER_LOGGED_IN   = 'USER_LOGGED_IN';
-export const USER_LOGGED_OUT  = 'USER_LOGGED_OUT';
+/*
+Login form validation.
+ */
+export const USER_LOGIN_FIELD_ERROR   = 'USER_LOGIN_FIELD_ERROR';
+export const USER_LOGIN_FIELD_NOERROR = 'USER_LOGIN_FIELD_NOERROR';
+export const USER_PASS_FIELD_ERROR    = 'USER_PASS_FIELD_ERROR';
+export const USER_PASS_FIELD_NOERROR  = 'USER_PASS_FIELD_NOERROR';
 
-export const initialState = {
-    "auth": {
-        "id": null,
-        "login": null,
-        "jwt": null
-    }
-};
+/*
+Login.
+ */
+export const USER_LOGIN_REQUEST    = 'USER_LOGIN_REQUEST';
+export const USER_LOGGED_IN        = 'USER_LOGGED_IN';
+export const USER_LOGIN_FALSE      = 'USER_LOGIN_FALSE';
+export const USER_LOGGED_OUT       = 'USER_LOGGED_OUT';
+
+/*
+Tree.
+ */
+export const GET_TREE_REQUEST = 'GET_TREE_REQUEST';
+export const GET_TREE_DONE    = 'GET_TREE_DONE';
+export const GET_TREE_FALSE   = 'GET_TREE_FALSE';
+export const ADD_TREE_PAGE    = 'ADD_TREE_PAGE';
+
+/*
+Edit form.
+ */
+export const EDIT_FORM_FIELD_ERROR    = 'EDIT_FORM_FIELD_ERROR';
+export const EDIT_FORM_FIELD_NO_ERROR = 'EDIT_FORM_FIELD_NO_ERROR';
+export const EDIT_FORM_SAVE_REQUEST   = 'EDIT_FORM_SAVE_REQUEST';
+export const EDIT_FORM_SAVE_DONE      = 'EDIT_FORM_SAVE_DONE';
+export const EDIT_FORM_SAVE_FALSE     = 'EDIT_FORM_SAVE_FALSE';
