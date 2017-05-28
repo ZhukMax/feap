e6808d0874c3e0b076364c87c6fca59452d576d7
Max Zhuk <zhukmax@ya.ru>
2017-05-28 21:07:34 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,20 +1,37 @@
 import React from 'react';
-import { browserHistory, Router } from 'react-router';
+import { browserHistory, Router, IndexRoute, Route } from 'react-router';
 import { syncHistoryWithStore } from 'react-router-redux';
 import { Provider } from 'react-redux';
 import configureStore from './store/configureStore';
-import initialState from './store/initialState';
-import routes from './routes';
 
-const store = configureStore(initialState());
+import App from './app';
+import NotFound from './components/errors/NotFound';
+import Dashboard from './components/dashboard';
+import List from './components/list';
+import Edit from './components/edit';
+import Filemanager from './components/filemanager';
+
+const store = configureStore();
 const history = syncHistoryWithStore(browserHistory, store);
 
-const feap = (
-    <Provider store={store}>
-        <Router history={history}>
-            {routes}
-        </Router>
-    </Provider>
-);
+class Feap extends React.Component {
+    render() {
+        let settings = this.props;
 
-export default feap;
+        return (
+            <Provider store={store}>
+                <Router history={history}>
+                    <Route path="/admin" component={App} settings={settings}>
+                        <IndexRoute component={Dashboard} />
+                        <Route path="/admin/media" components={Filemanager} />
+                        <Route path="/admin/:object" component={List}/>
+                        <Route path="/admin/:object/:id" component={Edit}/>
+                        <Route path='*' component={NotFound} />
+                    </Route>
+                </Router>
+            </Provider>
+        );
+    }
+}
+
+export default Feap;
