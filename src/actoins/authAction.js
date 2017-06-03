72141bdc28517017bb8961d1348da1cce8cad163
Max Zhuk <zhukmax@ya.ru>
2017-06-03 12:28:26 +0300

    Update authAction.js

--- src/actoins/authAction.js
+++ src/actoins/authAction.js
@@ -1,16 +1,78 @@
 import * as constants from '../constants';
+import post from '../helpers/ajax';
 
-export const loginAdmin = (payload) => {
+export function inputError(payload) {
+    return (dispatch) => {
+        let type;
+
+        if (payload.field === "login") {
+            type = constants.USER_LOGIN_FIELD_ERROR;
+        } else {
+            type = constants.USER_PASS_FIELD_ERROR;
+        }
+
+        dispatch({
+            type: type
+        });
+    };
+}
+
+export function inputNoError(payload) {
+    return (dispatch) => {
+        let type;
+
+        if (payload.field === "login") {
+            type = constants.USER_LOGIN_FIELD_NOERROR;
+        } else {
+            type = constants.USER_PASS_FIELD_NOERROR;
+        }
+
+        dispatch({
+            type: type
+        });
+    };
+}
+
+export function loginAdmin(payload) {
     return (dispatch) => {
         dispatch({
             type: constants.USER_LOGIN_REQUEST
         });
-        post(payload.token, '/api/index/index', payload.data);
-    }
-};
+
+        let response = post("/api/auth", "", payload.data);
+        response.end((err, res) => {
+            if (err) {
+                dispatch({
+                    type: constants.USER_LOGIN_FALSE,
+                    error: err
+                });
+            } else if (res.status !== 200) {
+                dispatch({
+                    type: constants.USER_LOGIN_FALSE,
+                    error: "Error on server side, error code: " + res.status
+                });
+            } else {
+                let data = JSON.parse(res.text);
+                if (data.status === "error") {
+                    dispatch({
+                        type: constants.USER_LOGIN_FALSE,
+                        error: data.message
+                    });
+                } else {
+                    dispatch({
+                        type: constants.USER_LOGGED_IN,
+                        data: data
+                    });
+                }
+            }
+        });
+    };
+}
 
 export function logoutAdmin() {
-    return {
-        type: constants.USER_LOGGED_OUT
+    return (dispatch) => {
+        dispatch({
+            type: constants.USER_LOGGED_OUT
+        });
     }
 }
