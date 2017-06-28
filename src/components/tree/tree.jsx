c428bd3659ce064c098692bd3f4ef630531cc220
Max Zhuk <zhukmax@ya.ru>
2017-06-28 19:53:13 +0300

    Create tree.jsx

---
+++ src/components/tree/tree.jsx
@@ -0,0 +1,71 @@
+import React from 'react';
+import { connect } from 'react-redux';
+import { Link } from 'react-router';
+import { getTreeItems, createPage } from '../../actions/treeAction';
+import { Nav, NavItem } from 'reactstrap';
+import { Faicon } from 'faicon';
+
+import './tree.css';
+
+const mapStateToProps = (state) => {
+    return {
+        token: state.auth.token,
+        tree: state.tree
+    };
+};
+
+class Tree extends React.Component {
+    componentWillMount() {
+        getTreeItems({"token": this.props.token})(this.props.dispatch);
+    }
+
+    treeMaker(items) {
+        return Object.keys(items).map(function (key) {
+            return (
+                <NavItem key={key.toString()}>
+                    <Link to={ "/admin/pages/" + key }>{ items[key].title }</Link>
+                    {
+                        items[key].children ?
+                            <Nav vertical>{ this.treeMaker(items[key].children) }</Nav>
+                            : ""
+                    }
+                </NavItem>
+            );
+        }, this);
+    }
+
+    addPageHandler() {
+        createPage({})(this.props.dispatch);
+    }
+
+    render() {
+        let treeItems = this.props.tree;
+        /*let treeItems = {
+            112: {"title": "Корпоративным клиентам"},
+            514: {"title": "О компании", "children": {
+                515: {"title": "Новости", "children": {
+                    516: {"title": "Новый аквафон скоро"},
+                    517: {"title": "Новый аквафон 2017"},
+                    518: {"title": "Уже ведем работы"}
+                }}
+            }},
+            64: {"title": "Бизнес с АКВАФОН"}
+        };*/
+
+        return (
+            <div className="b-tree-navbar">
+                <header>
+                    ___ Страницы сайта
+                    <a href="#" className="b-create-page" onClick={this.addPageHandler}><Faicon name="plus"/></a>
+                </header>
+                <Nav vertical className="b-tree-nav">
+                    { this.treeMaker(treeItems) }
+                </Nav>
+            </div>
+        );
+    }
+}
+
+Tree = connect(mapStateToProps)(Tree);
+
+export default Tree;
