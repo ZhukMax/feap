2f781d35e6629ca06069553356743276620091a3
Max Zhuk <zhukmax@ya.ru>
2017-07-04 22:59:37 +0300

    Create editReducer.js

---
+++ src/reducers/editReducer.js
@@ -0,0 +1,13 @@
+import * as constants from '../constants';
+
+const initialState = {
+};
+
+function auth(state = initialState, action) {
+    switch (action.type) {
+        default:
+            return state;
+    }
+}
+
+export default auth;
