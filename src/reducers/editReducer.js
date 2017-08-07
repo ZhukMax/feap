b22d0f5e84da38e08fe45535a20b6b2e11c0641a
Max Zhuk <zhukmax@ya.ru>
2017-08-08 02:35:10 +0300

    Update editReducer.js

--- src/reducers/editReducer.js
+++ src/reducers/editReducer.js
@@ -1,13 +1,35 @@
 import * as constants from '../constants';
 
 const initialState = {
+    "object": {},
+    "load": false,
+    "error": null
 };
 
-function auth(state = initialState, action) {
+function edit(state = initialState, action) {
     switch (action.type) {
+        case constants.EDIT_FORM_GET_DATA_REQUEST:
+            return Object.assign({}, state, {
+                load: true,
+                error: null
+            });
+
+        case constants.EDIT_FORM_GET_DATA_FALSE:
+            return Object.assign({}, state, {
+                load: false,
+                error: action.error
+            });
+
+        case constants.EDIT_FORM_GET_DATA_DONE:
+            return Object.assign({}, state, {
+                object: action.data,
+                load: false,
+                error: null
+            });
+
         default:
             return state;
     }
 }
 
-export default auth;
+export default edit;
