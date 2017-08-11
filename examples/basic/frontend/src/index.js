ff2ffd5d10fbed4b77f58af3ec59ef3f163af8a1
Max Zhuk <zhukmax@ya.ru>
2017-08-11 19:25:28 +0300

    Update index.js

--- examples/basic/frontend/src/index.js
+++ examples/basic/frontend/src/index.js
@@ -2,8 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Feap from 'feap';
 
+let navigationItems = {
+    "users": "Пользователи",
+    "media": "Медиа-файлы",
+    "polls": "Опросы"
+};
+
 const myApp = (
-    <Feap />
+    <Feap
+        navigationItems={navigationItems}
+    />
 );
 
 ReactDOM.render(myApp, document.getElementById('root'));
