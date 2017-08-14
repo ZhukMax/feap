788a1f0140f6f7444d36beda7c144aa639aa0403
Max Zhuk <zhukmax@ya.ru>
2017-08-14 05:40:09 +0300

    Update input.jsx

--- src/components/input/input.jsx
+++ src/components/input/input.jsx
@@ -1,36 +1,84 @@
 import React from 'react';
-import { Input, FormText } from 'reactstrap';
+import { Col, FormGroup, Label, Input } from 'reactstrap';
 
 export default class FormInput extends React.Component {
     render() {
-        let type = this.props.type;
-        let name = this.props.name;
+        let type = this.props.item.type;
+        let label = this.props.item.name;
+        let value = this.props.item.value;
         let id = this.props.id;
-        let value = this.props.value;
+        let name = this.props.name;
         let placeholder = this.props.placeholder;
         let className = this.props.class;
 
+        // Types we can accept
+        let types = [
+            'text',
+            'email',
+            'textarea',
+            'password',
+            'select',
+            'checkbox',
+            'number',
+            'url',
+            'datetime',
+            'date',
+            'time',
+            'color',
+            'search',
+            'file'
+        ];
+
+        // If we can't accept type
+        // then return text input type
+        if (types.indexOf(type) === -1) {
+            type = 'text';
+        }
+
         if (type === "select") {
             return (
-                <Input type="select" name={name} id={id} className={className}>
-                    {
-                        Object.keys(value).map((key, i) => {
-                            return (
-                                <option value={key} key={key}>{value[key]}</option>
-                            );
-                        })
-                    }
-                </Input>
+                <FormGroup row>
+                    <Label for={id} sm={2} className={className + "label"}>
+                        {label}
+                    </Label>
+                    <Col sm={10}>
+                        <Input type="select" name={name} id={id} className={className + "input"}>
+                            {
+                                Object.keys(value).map((key, i) => {
+                                    return (
+                                        <option value={key} key={key}>{value[key]}</option>
+                                    );
+                                })
+                            }
+                        </Input>
+                    </Col>
+                </FormGroup>
             );
-        } else if (type === "checkbox") {
+        } else if (type === "checkbox" || type === "radio") {
             return (
-                <Label check>
-                    <Input className={className} type="checkbox" defaultValue={value}/>{' ' + placeholder}
-                </Label>
+                <FormGroup check>
+                    <Label check>
+                        <Input className={className + "input"} type={type} defaultValue={value}/>{' ' + label}
+                    </Label>
+                </FormGroup>
+            );
+        } else if (type === 'file') {
+            return (
+                <FormGroup row>
+                    <Label for={id} className={className + "label"}>{label}</Label>
+                    <Input type={type} name={name} id={id} className={className + "input"}/>
+                </FormGroup>
             );
         } else {
             return (
-                <Input type={type} name={name} id={id} placeholder={placeholder} defaultValue={value} className={className}/>
+                <FormGroup row>
+                    <Label for={id} sm={2} className={className + "label"}>
+                        {label}
+                    </Label>
+                    <Col sm={10}>
+                        <Input type={type} name={name} id={id} placeholder={placeholder} defaultValue={value} className={className + "input"}/>
+                    </Col>
+                </FormGroup>
             );
         }
     }
