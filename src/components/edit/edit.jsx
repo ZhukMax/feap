389727270c0e17fc72191058468a958cb066d53c
Max Zhuk <zhukmax@ya.ru>
2017-08-23 05:40:53 +0300

    Update edit.jsx

--- src/components/edit/edit.jsx
+++ src/components/edit/edit.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import { connect } from 'react-redux';
-import { Container, Row, Col, Button, ButtonGroup, TabContent, TabPane, Nav, NavItem, NavLink, Form, FormGroup, Label, Input, FormText } from 'reactstrap';
+import { Container, Row, Col, Button, ButtonGroup, TabContent,
+    TabPane, Nav, NavItem, NavLink, Form } from 'reactstrap';
 import Loader from '../loader';
+import FormInput from "../input";
 import classnames from 'classnames';
-import { Faicon } from "faicon";
-import { getData, save } from '../../actions/editAction';
+import { getData, save, updateForm } from '../../actions/editAction';
 
 import './edit.css';
 
@@ -12,7 +13,9 @@ const mapStateToProps = (state) => {
     return {
         token: state.auth.token,
         load: state.edit.load,
-        object: state.edit.object
+        object: state.edit.object,
+        id: state.edit.id,
+        type: state.edit.type
     };
 };
 
@@ -20,22 +23,31 @@ class Edit extends React.Component {
     constructor(props) {
         super(props);
 
+        this.tabsMaker = this.tabsMaker.bind(this);
         this.toggle = this.toggle.bind(this);
         this.state = {
-            activeTab: '1'
+            activeTab: 't0',
+            id: null
         };
-    }
 
-    componentWillMount() {
         this.validation = this.validation.bind(this);
         this.saveHandler = this.saveHandler.bind(this);
         this.getForm = this.getForm.bind(this);
         this.getFormData = this.getFormData.bind(this);
-        this.getFormData();
+        this.update = this.update.bind(this);
+    }
+
+    getFormData() {
+        let data = {};
+        data.object = this.props.params.object;
+        data.id = this.props.params.id;
+
+        getData({"token": this.props.token, data})(this.props.dispatch);
     }
 
     toggle(tab) {
         if (this.state.activeTab !== tab) {
+            // TODO where is my lovely redux?
             this.setState({
                 activeTab: tab
             });
@@ -61,21 +73,43 @@ class Edit extends React.Component {
         }
     }
 
-    getFormData() {
-        let data = {};
-        data.object = this.props.params.object;
-        data.id = this.props.params.id;
-
-        getData({"token": this.props.token, data})(this.props.dispatch);
+    tabsMaker(items) {
+        if(Object.prototype.toString.call(items) === '[object Object]') {
+            let e = this;
+            return Object.keys(items).map((key, i) => {
+                return (
+                    <NavItem key={i}>
+                        <NavLink className={classnames({active: e.state.activeTab === 't' + i})}
+                                 onClick={() => {
+                                     e.toggle('t' + i);
+                                 }}>
+                            { key }
+                        </NavLink>
+                    </NavItem>
+                );
+            });
+        }
     }
-
-    tabsMaker() {}
 
     formMaker(items) {
         if(Object.prototype.toString.call(items) === '[object Object]') {
-            return Object.keys(items).map(function (key) {
-                if (items[key]) {
-                    console.log(key + ":" + items[key]);
+            return Object.keys(items).map((key, i) => {
+                if(Object.prototype.toString.call(items[key]) === '[object Object]') {
+                    return (
+                        <TabPane tabId={"t" + i} key={i}>
+                            {
+                                Object.keys(items[key]).map((key2, y) => {
+                                    return (
+                                        <Row key={y}>
+                                            <Col sm="12">
+                                                <FormInput item={items[key][key2]} name={key2} id={key2} class="b-edit-"/>
+                                            </Col>
+                                        </Row>
+                                    );
+                                })
+                            }
+                        </TabPane>
+                    );
                 }
             });
         }
@@ -83,7 +117,7 @@ class Edit extends React.Component {
 
     getHead() {
         return (
-            <Container className="b-edit-header">
+            <Container className="b-header">
                 <Row>
                     <Col>
                         <header>
@@ -103,67 +137,21 @@ class Edit extends React.Component {
 
     getForm() {
         let object = this.props.object;
-        // console.log(object);
         this.formMaker(object);
 
         return (
-            <div className="b-edit-content-wrapper">
-                <Container className="b-edit-content">
+            <div className="b-content-wrapper">
+                <Container className="b-content">
                     <Row>
                         <Col>
-                            <Nav tabs>
-                                <NavItem>
-                                    <NavLink className={classnames({active: this.state.activeTab === '1'})}
-                                             onClick={() => {
-                                                 this.toggle('1');
-                                             }}>
-                                        Tab1
-                                    </NavLink>
-                                </NavItem>
-                                <NavItem>
-                                    <NavLink className={classnames({active: this.state.activeTab === '2'})}
-                                             onClick={() => {
-                                                 this.toggle('2');
-                                             }}>
-                                        Tab2
-                                    </NavLink>
-                                </NavItem>
-                            </Nav>
-                            <TabContent activeTab={this.state.activeTab} className="b-edit-tab-content">
-                                <TabPane tabId="1">
-                                    <Row>
-                                        <Col sm="6">
-                                            <FormGroup row>
-                                                <Label for="exampleEmail" sm={2}
-                                                       className="b-edit-label">Email</Label>
-                                                <Col sm={10}>
-                                                    <Input type="email" name="email" id="exampleEmail"
-                                                           placeholder="with a placeholder"
-                                                           className="b-edit-input"/>
-                                                </Col>
-                                            </FormGroup>
-                                        </Col>
-                                        <Col sm="6">
-                                            <FormGroup row>
-                                                <Label for="exampleEmail2" sm={2}
-                                                       className="b-edit-label">Email</Label>
-                                                <Col sm={10}>
-                                                    <Input type="email" name="email2" id="exampleEmail2"
-                                                           placeholder="with a placeholder"
-                                                           className="b-edit-input"/>
-                                                </Col>
-                                            </FormGroup>
-                                        </Col>
-                                    </Row>
-                                </TabPane>
-                                <TabPane tabId="2">
-                                    <Row>
-                                        <Col sm="12">
-                                            <h4>Tab 2 Contents</h4>
-                                        </Col>
-                                    </Row>
-                                </TabPane>
-                            </TabContent>
+                            <Form id={"b-edit-form"}>
+                                <Nav tabs>
+                                    { this.tabsMaker(object) }
+                                </Nav>
+                                <TabContent activeTab={this.state.activeTab} className="b-edit-tab-content">
+                                    { this.formMaker(object)}
+                                </TabContent>
+                            </Form>
                         </Col>
                     </Row>
                 </Container>
@@ -171,9 +159,22 @@ class Edit extends React.Component {
         );
     }
 
+    update() {
+        if (this.props.params.id !== this.props.id || this.props.params.object !== this.props.type) {
+            let id = this.props.params.id;
+            let type = this.props.params.object;
+            updateForm({"id": id, "type": type})(this.props.dispatch);
+
+            this.getFormData();
+        }
+    }
+
     render() {
+        // TODO В консоли варнинг, починить
+        this.update();
+
         return (
-            <Container className="b-edit-container">
+            <Container className="b-container">
                 { this.getHead() }
                 {
                     this.props.load ? <Loader/> : this.getForm()
